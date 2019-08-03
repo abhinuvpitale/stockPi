dd44a9498705dc2a1996f592dba82a5f826f7aa3
abhinuv-dev-auto <abhinuvpitale@gmail.com>
2019-08-02 22:39:05 -0700

    first commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install dependencies
+sudo apt install apache jq
+
+# install ticker.sh
+curl -o ticker.sh https://raw.githubusercontent.com/pstadler/ticker.sh/master/ticker.sh
---
+++ manageStockList.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def ask_user():
+    print("Hey, Welcome to StockPi!")
+    if os.path.exists("stocks.json"):
+
+if __name__ == "__main__":
+    ask_user()
